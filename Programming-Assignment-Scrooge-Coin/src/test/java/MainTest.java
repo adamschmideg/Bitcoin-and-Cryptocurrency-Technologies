02298cb3e649c7a075ca97c9a474436ac429f1b6
Adam Schmideg <offcode@Holacrator.local>
2017-03-29 12:05:36 +0200

    Empty gradle project

---
+++ Programming-Assignment-Scrooge-Coin/src/test/java/MainTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by offcode on 2017. 03. 29..
+ */
+public class MainTest {
+
+    @Test
+    public void testSomething() {
+        assertEquals("Nothing", 1, 1);
+    }
+}
